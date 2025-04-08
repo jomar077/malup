e6ada467c456b2138e0c83552a9df579445fa91e
jomar077 <145532158+jomar077@users.noreply.github.com>
2025-04-08 16:15:26 +0800

    activity 4

---
+++ about.php
@@ -0,0 +1,24 @@
+  <!-- about section -->
+  <section id="about">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-6">
+          <h3>LOREM IPSUM</h3>
+          <h2>Lorem Ipsum</h2>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            It has survived not only five centuries, but also the leap into electronic typesetting, remaining
+            essentially unchanged.
+            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          </p>
+          <a href="b" class="btn btn-malup">ABOUT</a>
+        </div>
+        <div class="col-lg-6">
+          <img src="mal3.jpg" alt="">
+        </div>
+      </div>
+    </div>
+
+  </section>.
